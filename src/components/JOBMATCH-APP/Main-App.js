1a4da2aeb6c9cbcaf5e43e310cb3873809c5d335
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2024-05-25 12:13:25 +0300

    useEffect

--- src/components/JOBMATCH-APP/Main-App.js
+++ src/components/JOBMATCH-APP/Main-App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import RankingPro from "./RankingPro";
 import Degree from "./Degree";
 import Major from "./Major";
